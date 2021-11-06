e01a14265a4ff5bad83bbc221ce07c354a4ee0ee
DersonM <wanderson.derson.m@gmail.com>
2021-11-06 18:09:02 -0300

    Initial commit

---
+++ Exercicios/ex001/teste.js
@@ -0,0 +1,9 @@
+
+for (var index = 0; index < n; index ++) {
+    var linhaDeAsterisco = '' ;
+
+    for (var linha = 0; linha < n; linha ++ ) {
+        linhaDeAsterisco = linhaDeAsterisco + '*';
+    }
+    console.log(linhaDeAsterisco)
+}
